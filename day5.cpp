bfe6fd11def2944beead6b5030dfd483c9c20104
Suraj Kumar <surajgenius66678@gmail.com>
2026-06-13 23:43:00 +0530

    DSA Day 5 prctice

---
+++ day5.cpp
@@ -0,0 +1,35 @@
+// #include <iostream>
+// using namespace std;
+// void dectobin(int n);
+// int main (){
+//     int n =64;
+//     dectobin(n);
+//     return 0;
+// }
+// void dectobin(int n){
+//     int pro =1;
+//     int bin=0;
+// for (int i=n;i>0;i=i/2){
+//     bin+=i%2*pro;
+//     pro = pro*10;
+// }
+// cout << bin ;
+// }
+
+// # include <iostream>
+// using namespace std;
+// void decitobin (int n);
+// int main(){
+//     int n =1000101;
+//     decitobin(n);
+//     return 0;
+// }
+// void decitobin (int n){
+//     int pow =1;
+//     int decimal = 0;
+//     for (int i=n;i>0;i=i/10){
+//         decimal+=(i%10)*pow;
+//         pow=pow*2;
+//     }
+//     cout << decimal ;
+// }
