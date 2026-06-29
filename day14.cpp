4cd5db5943cdf605e4fea2c2a5ef5349d5d006d6
Suraj Kumar <surajgenius66678@gmail.com>
2026-06-29 09:05:24 +0530

    DSA DAY 14

---
+++ day14.cpp
@@ -0,0 +1,57 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10;
+//     cout << &a;// & represent address
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10;
+//     int * ptr = &a;
+//     cout << ptr ;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 10;
+//     int * ptr = &a;
+//     int **prtptr=&ptr;
+//     cout << ptr ;
+//     cout << *prtptr ;
+//     return 0;
+// } 
+
+
+// NOTE-- "*"give the value present at stored address
+// NOTE-- "&"gives the address where the value is stored.
+
+
+
+// NULL POINTER 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+    //     int *ptr = NULL;
+    //     cout << ptr ;
+    //     return 0;
+    // } 
+
+    
+    #include<iostream>
+    using namespace std;
+    int main(){
+    int arr[] = {1,2,3,4,5};
+    cout << arr << endl ; // it stores the address of 0th index of array
+    return 0;
+    }
+    // #include<iostream>
+    // using namespace std;
+    // int main(){
+    
+    //     return 0;}
